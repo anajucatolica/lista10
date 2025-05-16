9081cd247ad988fde28e5bbe5e8938a5cf56c50f
Ana Soares <a.vicente@catolicasc.edu.br>
2025-05-16 14:56:11 -0300

    Update 4_Ana.c

--- 4_Ana.c
+++ 4_Ana.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
-int meu_strcmp(char *str1, char *str2) {
+int meu_strcmp(char *s1, char *s2) {
     int i = 0;
-    while (str1[i] != '\0' && str2[i] != '\0') {
-        if (str1[i] != str2[i]) return 0;
+    while (s1[i] != '\0' && s2[i] != '\0') {
+        if (s1[i] != s2[i]) return 0;
         i++;
     }
-    return (str1[i] == '\0' && str2[i] == '\0') ? 1 : 0;
+    return (s1[i] == '\0' && s2[i] == '\0') ? 1 : 0;
 }
 
 int main() {
-    char a[] = "teste";
-    char b[] = "teste";
-    printf("comparação: %d\n", meu_strcmp(a, b));
+    char a[100], b[100];
+    printf("digite a primeira string: ");
+    scanf("%s", a);
+    printf("digite a segunda string: ");
+    scanf("%s", b);
+
+    if (meu_strcmp(a, b))
+        printf("as strings são iguais.\n");
+    else
+        printf("as strings são diferentes.\n");
+
     return 0;
 }
